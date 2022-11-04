4abf50f5dc803d0fcb38a26087e54bd82b2d1cbd
李代伟 <lidwn@digitalchina.com>
2022-11-04 15:18:33 +0800

    添加启动类,以及初始化相关文件

---
+++ src/main/java/com/ldw/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.ldw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname BackendApplication
+ * @Description TODO
+ * @Date 2022/11/4 15:11
+ * @Created by ldw
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
